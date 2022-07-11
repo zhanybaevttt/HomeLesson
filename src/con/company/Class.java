c8a4f85f1776e2f8170c230a48cacb59fcc4e859
programist <zhanybaevttt@gmail.com>
2022-07-11 19:59:33 +0300

    commit

--- src/con/company/Class.java
+++ src/con/company/Class.java
@@ -1,2 +1,43 @@
-package con.company;public class Class {
-}
+package con.company;
+
+public class Class {
+    private int nom;
+    private String soz;
+    private int[] massiv;
+
+    public Class(int nom,String soz ,int[] massiv) {
+        this.nom=nom;
+        this.soz=soz;
+        this.massiv=massiv;
+    }
+
+
+    public int getNom() {
+        return nom;
+    }
+
+
+
+
+    public String getSoz() {
+        return soz;
+    }
+
+
+
+
+    public int[] getMassiv() {
+        return massiv;
+    }
+    public void array(int[] massiv){
+        for (int i = 0; i <massiv.length; i++) {
+            System.out.println(massiv[i]);
+        }
+
+    }
+
+
+    }
+
+
+
--- src/con/company/Main.java
+++ src/con/company/Main.java
@@ -1,5 +1,20 @@
+package con.company;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Class clas = new Class(13256,"java",new int[]{1,2});
+        System.out.println(clas.getNom());
+        System.out.println(clas.getSoz());
+        clas.array(clas.getMassiv());
+
+
+
+
+
+
+
+
     }
 }
